718a10d32e63367908ec18bbd74e6ee452c88282
Amin Bakhtiyari <aminmail69@gmail.com>
2019-10-22 11:35:10 +0330

    update gradle and gitignore

--- app/src/main/java/com/kmanikumarreddy/bluetoothhomeautomation/MainActivity.java
+++ app/src/main/java/com/kmanikumarreddy/bluetoothhomeautomation/MainActivity.java
@@ -6,11 +6,12 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "BluetoothHomeAutomation";
 
     private BluetoothSocket socket;
-    private BluetoothDevice device;
     private OutputStream os;
     private BluetoothAdapter mBluetoothAdapter;
     public String deviceName;
@@ -33,12 +33,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nameView = (TextView)findViewById(R.id.controllerName);
+        nameView = findViewById(R.id.controllerName);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(),R.string.Bluetooth_NA, Toast.LENGTH_LONG).show();
-        }
-        else if (!mBluetoothAdapter.isEnabled()) {
+            Toast.makeText(getApplicationContext(), R.string.Bluetooth_NA, Toast.LENGTH_LONG).show();
+        } else if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivity(enableBtIntent);
         }
@@ -47,12 +46,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences preferences = getSharedPreferences("preferences",MODE_PRIVATE);
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
         deviceName = preferences.getString("controllerName", "NA");
         deviceAddress = preferences.getString("controllerAddress", "");
         nameView.setText(deviceName);
-        if(!deviceAddress.equals("")) {
-            device = mBluetoothAdapter.getRemoteDevice(deviceAddress);
+        if (!deviceAddress.equals("")) {
+            BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(deviceAddress);
             try {
                 socket = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
             } catch (IOException e) {
@@ -77,9 +76,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "getting output stream failed: " + e.getMessage() + "\n");
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
             }
-        }
-        else {
-            Toast.makeText(getApplicationContext(),"Connect to a device",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Connect to a device", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -91,33 +89,32 @@ public class MainActivity extends AppCompatActivity {
                 os.flush();
             } catch (IOException e) {
                 Log.e(TAG, "flushing output stream failed: " + e.getMessage() + "\n");
-                Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
             }
         }
         try {
-            if(socket != null)
+            if (socket != null)
                 socket.close();
         } catch (IOException e) {
             Log.e(TAG, "closing socket failed: " + e.getMessage() + "\n");
-            Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
     public void sendSignal(View view) {
-        if(socket.isConnected()) {
+        if (socket.isConnected()) {
             try {
                 os.write(view.getResources().getResourceName(view.getId()).getBytes());
             } catch (IOException e) {
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
             }
-        }
-        else {
+        } else {
             Toast.makeText(getBaseContext(), "Connect to the seleceted bluetooth device first", Toast.LENGTH_LONG).show();
         }
     }
 
     public void selectFromList(View view) {
-        Intent intent = new Intent(this,SelectController.class);
+        Intent intent = new Intent(this, SelectController.class);
         startActivity(intent);
     }
 }
