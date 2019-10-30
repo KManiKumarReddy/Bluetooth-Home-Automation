5b597a839583f5b4c0a002144b660777ef81b19b
TheLazyCode <gauravshukla052@gmail.com>
2019-10-31 01:36:56 +0530

    Made the code more understandable

--- bluetoothHomeAutomation.ino
+++ bluetoothHomeAutomation.ino
@@ -1,4 +1,6 @@
 #include <SoftwareSerial.h>
+// Attach pin 13 to the led
+// Insert the HC-05 into the Rx and Tx terminal in the arduino
 
 SoftwareSerial hc05(10, 11);
 int signl = 0;
@@ -6,7 +8,7 @@ int currentState = 0;
 
 void setup() {
   pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  digitalWrite(13, LOW);                        // by default setting led off.
   hc05.begin(9600);
 }
 
@@ -14,8 +16,8 @@ void loop() {
   if (hc05.available()) {
     signl = hc05.read();
   }
-  if (signl == 'a' && !currentState) {
-    digitalWrite(13, HIGH);
+  if (signl == 'a' && !currentState) {         // checking if the HC-05 have sent a or not if yes then led will glow
+    digitalWrite(13, HIGH);                                                         
     currentState = 1;
   }
   if (signl == 'b' && currentState) {
